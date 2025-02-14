df6b6909fc0b57df8ec5a22d25be70dff6185043
rustic-d3 <dumbravarustic454@gmail.com>
2025-02-14 14:29:49 +0200

    Self collition added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(10, 10), Vector2(11,10), Vector2(12,10)]
-        self.direction = Vector2(1, 0)
+        self.direction = Vector2(-1, 0)
     def draw_snake(self):
         for blocks in self.body:
             block = pygame.Rect(blocks.x * cell_size, blocks.y * cell_size, cell_size, cell_size)
@@ -31,17 +31,25 @@ class MAIN:
     def __init__(self):
         self.snake = SNAKE()
         self.fruit = FRUIT()
+        self.new_block = False
     def update(self):
         self.fruit.draw_fruit()
         self.snake.draw_snake()
 
     def check_for_food(self):
+        self.new_block = False
         if self.snake.body[0] == self.fruit.pos:
             self.fruit.x = random.randint(0, cell_nums-1)
             self.fruit.y = random.randint(0, cell_nums-1)
             self.snake.body.insert(0, self.fruit.pos)
             self.fruit.pos = Vector2(self.fruit.x, self.fruit.y)
-            
+            self.new_block = True
+    def check_for_collision(self):
+        if self.snake.body[0] in self.snake.body[1:] and self.new_block == False:
+            print("Collision has been detected") 
+
+        
+
 
 
 
@@ -73,6 +81,7 @@ while running:
         if event.type == MOVE_TRIGGER:
             main.snake.move_snake()
             main.check_for_food()
+            main.check_for_collision()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 main.snake.direction = Vector2(0, -1)
